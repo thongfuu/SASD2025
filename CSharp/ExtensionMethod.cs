ccf42623a3bf1d6c64f331cd3bb9de2404ea2088
Korawich C. <korawichfaa@gmail.com>
2026-01-30 16:30:02 +0700

    Add CSharp

---
+++ CSharp/ExtensionMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp;
+
+public static class StringUtil
+{
+    public static string Half(this string s)
+    {
+        if (string.IsNullOrEmpty(s)) return string.Empty;
+
+        // หาจุดกึ่งกลาง (หาร 2)
+        int startIndex = s.Length / 2;
+
+        // คืนค่าตั้งแต่จุดกึ่งกลางจนจบ String
+        return s.Substring(startIndex);
+    }
+}
+
